c086b1cca9f10e75108c8d896cfb802f32bb3813
Jhonny Castro Clavijo <johnny.castro@misena.edu.co>
2022-05-05 16:48:50 -0500

    Get Notes
    Fetch all notes from the backend

--- src/components/ListOfToDo.jsx
+++ src/components/ListOfToDo.jsx
@@ -1,8 +1,27 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { Store } from "./StoreProvider";
 
 const ListOfToDo = () => {
   const { state, dispatch } = useContext(Store);
+
+  useEffect(() => {
+    let listOfNote = fetchAllNotes().then(
+      notes => {
+        let action = {
+          type: 'get-notes',
+          payload: notes
+        }
+
+        dispatch(action)
+      }
+    )
+  }, [])
+
+  const fetchAllNotes = async() => {
+    let response = await fetch('http://localhost:8081/api/v1/get/note')
+    let data = await response.json()
+    return data
+  }
 
   const onCheckBox = (event, note) => {
     const checked = event.currentTarget.checked;
